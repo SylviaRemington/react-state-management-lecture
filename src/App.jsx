1e7249e6238651eafc06dfb64ec956c8d50fbbbd
SylviaRemington <sylviaremington@gmail.com>
2025-07-23 15:11:39 -0700

    importing the useState hook function from React & adding state with state variable and state setter function & console logging

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+// importing the useState() hook from React at top of App.jsx
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState();
+  console.log('Our isDarkMode state value is:', isDarkMode);
+  // Make sure console.log is outside of the return statement.
+  
   return (
     <h1>Hello world!</h1>
   );
 };
 
 export default App;
+
