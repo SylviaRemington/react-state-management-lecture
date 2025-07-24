22883e7ae0fce603701fd071449fcd19c0809944
SylviaRemington <sylviaremington@gmail.com>
2025-07-23 21:09:57 -0700

    adding notes about useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState();
+  // Setting up state - state can be a boolean, a string, a number, an array of objects, a piece of data that you are using to control keeping some data on the screen & that data can be updated.
+  // useState() is invoking the useState function & helping us get our return values.
+  // useState() returns an array & pulls out the 2 variables of that array in its corresponding positions.
+  // ! need to initialize dark mode to a value, which now is false.
+  const [isDarkMode, setIsDarkMode] = useState(false);
   console.log('Our isDarkMode state value is:', isDarkMode);
   // Make sure console.log is outside of the return statement.
-  
+
   return (
     <h1>Hello world!</h1>
   );
